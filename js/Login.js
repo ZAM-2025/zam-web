07d741d20344f4bb5248e1d9d03886a5597f3d4a
Matteo Forlani <matteo@matthew5pl.net>
2025-03-10 20:58:24 +0100

    aggiunto captcha wip

--- js/Login.js
+++ js/Login.js
@@ -1,4 +1,6 @@
 // TODO: Implementare vera validazione
+var __captchaID = null;
+
 function ValidateLogin(user, pass, elem) {
     if (user && pass) {
         if(elem != null && elem != undefined) {
@@ -28,11 +30,16 @@ function RedirHome() {
 function SendLogin(data) {
     data.preventDefault();
 
+    if(__captchaID == null) {
+        alert("Captcha non inserito!");
+        return;
+    }
+
     let username = data.target.username.value;
     let password = data.target.password.value;
 
-    var auth = new ZAMAuth();;
-    auth.auth(username, password, true, (response) => {
+    var auth = new ZAMAuth();
+    auth.auth(username, password, __captchaID, true, (response) => {
         if(response.success) {
             RedirHome();
         } else {
@@ -45,6 +52,19 @@ window.onload = () => {
     var userElem = document.getElementById("username");
     var passElem = document.getElementById("password");
     var submitButton = document.getElementById("zam-submit");
+
+    var session = new CaptchaSession("http", "localhost:8080", "lib/zamcaptcha-js", (data) => {
+        console.log(data);
+        __captchaID = null;
+
+        if(data.success) {
+            __captchaID = data.id;
+        }
+    });
+
+    session.setFetchCallback(() => {
+        __captchaID = null;
+    });
 
     setInterval(() => {
         ValidateLogin(userElem.value, passElem.value, submitButton);
